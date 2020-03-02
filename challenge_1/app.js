e68df73eeebe8e6ab7ec13c87afbe34bb63cd355
tomjimseng <tomjimseng@gmail.com>
2020-03-02 13:29:52 -0800

    Tic Tac Toe added board with X and O

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,22 @@
+console.log('Hello World');
 
+function changeText(id) {
+  if (id.innerHTML = '  ') {
+    var value = change(string);
+    id.innerHTML = value;
+  } else {
+    console.log('invalid move');
+  }
+}
 
+var string = 'O';
+var change = function(value) {
+if (string === 'X') {
+  string ='O';
+  return string;
+} else {
+  string = 'X';
+  return string;
+}
+return string;
+}
