c278fc27771cdc3d1ad815e8238cec488c68f242
tomjimseng <tomjimseng@gmail.com>
2020-03-03 11:02:19 -0800

    Connected server to port

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const app = express()
+const port = 3001
 
+app.use(express.static('client'));
+app.post('/upload_json', (req, res) => res.send(req.body));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
