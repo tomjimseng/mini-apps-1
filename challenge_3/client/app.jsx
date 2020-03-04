03f5fe80d7ae4eae935100338863f68aa9684a72
tomjimseng <tomjimseng@gmail.com>
2020-03-04 13:38:19 -0800

    Set up package.json and app.jsx set up

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,17 @@
+console.log('Hello World');
+
+const React = require('react')
+const express = require('express')
+
+class App extends React.Component {
+  contructor(props){
+    super(props);
+
+  }
+
+  render() {
+    return console.log('Hello World', App)
+  }
+}
 
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,2 @@
-
+app.use(express.static('public')
 
