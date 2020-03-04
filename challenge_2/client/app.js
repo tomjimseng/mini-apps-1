620ba6cf71bb82e4803b5e15116243e993113940
tomjimseng <tomjimseng@gmail.com>
2020-03-04 10:14:17 -0800

    small changes and implementation of multer

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,6 @@
+const express = require('express');
+
+var app = express();
+
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const multer = require('multer');
 const app = express()
 const port = 3001
 
 app.use(express.static('client'));
-app.post('/upload_json', (req, res) => res.send(req.body));
+
+app.use(multer({dest:'./upload_json/'});)
+app.post('/upload_json', (req, res) => res.send('post request to homepage'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
